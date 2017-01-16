0b9b88ef470da3e14401fc4079d1844a8f5c210e
John Pressman <jmpressman@gmail.com>
2017-01-16 18:43:06 -0500

    added middleware

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ app.listen(3000, function () {
   console.log('Server listening');
 });
 
+app.use('/', function(req, res, next) {
+  console.log('Incoming: ' + req.method + '; ' + req.url);
+  next();
+});
+
 app.get('/', function(req, res, next) {
   res.send('Welcome. I\'m useless');
+});
+
+app.get('/news', function(req, res, next) {
+  res.send('Here\'s the news');
 });
