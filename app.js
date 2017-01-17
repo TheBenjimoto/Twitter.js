fccc41a7ed3e23540edcfdab8842a1e4f75a289c
John Pressman <jmpressman@gmail.com>
2017-01-17 10:46:54 -0500

    incorporated nunjucks

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
+const volleyball = require( 'volleyball' );
+const nunjucks = require( 'nunjucks' );
+
+let locals = {
+  title: 'Fun Times',
+  people: [
+    {name: 'Ben'},
+    {name: 'Jake'}
+  ]
+};
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', { noCache: true });
+
+// nunjucks.render('index.html', locals, function(err, output) {
+//   if (err) throw err;
+//   console.log(output);
+// });
 
 app.listen(3000, function () {
   console.log('Server listening');
 });
 
-app.use('/special/', function(req, res, next) {
-	console.log("you reached my special area.");
-	next();
-})
-
-app.use('/', function(req, res, next) {
-  console.log('Incoming: ' + req.method + '; ' + req.url);
-  next();
-});
+app.use(volleyball);
 
 app.get('/', function(req, res, next) {
-  res.send('Welcome. I\'m useless');
+  res.render('index', locals);
 });
 
 app.get('/news', function(req, res, next) {
